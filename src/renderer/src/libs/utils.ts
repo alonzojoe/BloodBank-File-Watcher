22a2e249f1aca7d1e957aabcbf4837e4f8b69fcf
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-03-13 10:12:02 +0800

    feat: create extract file name function to extract RenderNumber, BloodBank TemplateCode and PX Name

--- src/renderer/src/libs/utils.ts
+++ src/renderer/src/libs/utils.ts
@@ -2,13 +2,14 @@ type ReturnValue = [string, string, string]
 
 export const extractFileName = <T extends string>(fileName: T): ReturnValue => {
   const parts = fileName.split('&')
-  const getLisTemplateCode = `LAB-FM-${parts[4]}`
-  const getRenderNumberWithExtension = parts[5]
-  const getRenderNumber = getRenderNumberWithExtension.split('.')[0]
+  const extractNames = parts[5]
+  const extractParts = extractNames.split('^')
+  const renderNumber = extractParts[0]
+  const templateCode = extractParts[1]
 
   const rawPatientName = parts[3]
   const patientNameParts = rawPatientName.split('^')
   const formattedPatientName = `${patientNameParts[0]}, ${patientNameParts.slice(1).join(' ')}`
 
-  return [getLisTemplateCode, getRenderNumber, formattedPatientName]
+  return [renderNumber, templateCode, formattedPatientName]
 }
